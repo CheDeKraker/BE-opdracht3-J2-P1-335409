4c4996d8611d34bdfafd6442521aa1fad4a354f0
BolleTito335409 <chemichaeldekraker@gmail.com>
2024-01-24 11:35:49 +0100

    Opdracht 3 herkansing af

---
+++ app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'BE_opdracht1_J2_P1');
+define('URLROOT', 'http://www.be-p2-opdracht3.com/');
+ 
